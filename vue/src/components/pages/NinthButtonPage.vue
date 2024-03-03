d05c92524857673ac5344db62900f0fba3ef71f3
Hass <85948101+has-s@users.noreply.github.com>
2024-03-03 22:56:03 +0300

    Кнопка 9 (Issue #10) (#29)
    * Refactor for variant
    
    * Refactor for variant
    
    * Реализация кнопок за исключением состояния active (мне эти кнопки уже снятся)
    
    * Реализация кнопок за исключением состояния active (мне эти кнопки уже снятся)
    
    * Реализация кнопок за исключением состояния active (мне эти кнопки уже снятся)
    
    * Исправление структуры и читаемости кода
    
    * Удаление сегмента data (), добавление пустых строк в конце кода
    
    * Возврат пустой строки в App и добавление пробела в button:true
    
    * Исправление class
    
    ---------
    
    Co-authored-by: hass <auttenberg@icloud.com>

---
+++ vue/src/components/pages/NinthButtonPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <PageLayout>
+    <h1 class="title">Button 9 Issue #10 </h1>
+    <div class="grid-container">
+      <NinthButton />
+      <NinthButton :disabled="true" />
+    </div>
+  </PageLayout>
+</template>
+
+<script>
+import PageLayout from '../parts/PageLayout';
+import NinthButton from "@/components/ui/buttons/NinthButton.vue";
+
+export default {
+  components: {
+    PageLayout,
+    NinthButton
+  },
+}
+</script>
+
+<style>
+.title {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+  grid-gap: 20px;
+}
+</style>
---
+++ vue/src/components/ui/buttons/NinthButton.vue
@@ -0,0 +1,68 @@
+<template>
+  <button
+    class="button"
+    :disabled="this.disabled"
+  >
+    <slot>
+      Button
+    </slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'NinthButton',
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    active: {
+      type: Boolean,
+      default: false
+    },
+  }
+}
+</script>
+
+<style scoped lang="less">
+.button {
+  background-color: #ebeef2;
+  color: #7187BD;
+  border: 1px solid #ebeef2;
+  border-radius: 8px;
+  padding: 12px 20px;
+  box-shadow: -6px -6px 16px rgba(255, 255, 255, 0.8), 3px  3px 16px rgba(4, 4, 47, 0.2);
+  transition: all 0.3s ease;
+  cursor: pointer;
+  font-weight: bold;
+  width: 110px;
+
+  &:active {
+    box-shadow: inset -3px -3px 16px rgb(235, 238, 242), inset 2px 2px 16px rgba(3, 3, 47, 0.2);
+    background-color: #e4e6ee;
+    color: #88a2fc;
+  }
+
+  &:focus {
+    color: #88a2fc;
+  }
+
+  &:hover {
+    background-color: #edf0f4;
+    color: #b5c0e0;
+  }
+
+  &.pressed {
+    background-color: #e4e6ee;
+    color: #5971ab;
+    box-shadow: inset -3px -3px 16px rgb(235, 238, 242), inset 2px 2px 16px rgba(3, 3, 47, 0.2);
+  }
+
+  &:disabled {
+    color: #bbbbbb;
+    cursor: not-allowed;
+    box-shadow: -6px -6px 16px rgba(255, 255, 255, 0.05), 6px 6px 16px rgba(0, 0, 0, 0.05);
+  }
+}
+</style>
