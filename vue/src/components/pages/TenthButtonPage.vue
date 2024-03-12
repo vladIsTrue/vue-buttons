6d65c8497c35badffa3029e7e919ccc9534f5647
vladIsTrue <vladislavurcev@gmail.com>
2024-03-12 19:21:30 +0300

    refactor

--- vue/src/components/pages/TenthButtonPage.vue
+++ vue/src/components/pages/TenthButtonPage.vue
@@ -2,24 +2,51 @@
   <PageLayout>
     <div class="flex-container">
       <div>
-        <TenthButton @click="() => increment(0)" color="red" rounded="true"> 
+        <TenthButton 
+          @click="() => increment(0)"
+          color="red" 
+          rounded="true"
+        >
           Strawberry (<span>{{ getButtonCounts[0] }}</span>)
         </TenthButton>
-        <TenthButton @click="() => increment(1)" color="green" rounded="true">
+        <TenthButton
+          @click="() => increment(1)"
+          color="green"
+          rounded="true"
+        >
           Apple (<span>{{ getButtonCounts[1] }}</span>)
         </TenthButton>
-        <TenthButton @click="() => increment(2)" color="blue" rounded="true"> 
+        <TenthButton
+          @click="() => increment(2)"
+          color="blue"
+          rounded="true"
+        >
           Blueberry (<span>{{ getButtonCounts[2] }}</span>)
         </TenthButton>
       </div>
       <div>
-        <TenthButton @click="() => increment(3)" color="red" bordered="true" rounded="true"> 
+        <TenthButton
+          @click="() => increment(3)"
+          color="red"
+          bordered="true"
+          rounded="true"
+        >
           Strawberry (<span>{{ getButtonCounts[3] }}</span>)
         </TenthButton>
-        <TenthButton @click="() => increment(4)" color="green" bordered="true" rounded="true"> 
+        <TenthButton
+          @click="() => increment(4)"
+          color="green"
+          bordered="true"
+          rounded="true"
+        >
           Apple (<span>{{ getButtonCounts[4] }}</span>)
         </TenthButton>
-        <TenthButton @click="() => increment(5)" color="blue" bordered="true" rounded="true"> 
+        <TenthButton
+          @click="() => increment(5)"
+          color="blue"
+          bordered="true"
+          rounded="true"
+        >
           Blueberry (<span>{{ getButtonCounts[5] }}</span>)
         </TenthButton>
       </div>
@@ -35,13 +62,25 @@
         </TenthButton>
       </div>
       <div>
-        <TenthButton @click="() => increment(9)" color="red" bordered="true"> 
+        <TenthButton
+          @click="() => increment(9)"
+          color="red"
+          bordered="true"
+        >
           Strawberry (<span>{{ getButtonCounts[9] }}</span>)
         </TenthButton>
-        <TenthButton @click="() => increment(10)" color="green" bordered="true"> 
+        <TenthButton
+          @click="() => increment(10)"
+          color="green"
+          bordered="true"
+        >
           Apple (<span>{{ getButtonCounts[10] }}</span>)
         </TenthButton>
-        <TenthButton @click="() => increment(11)" color="blue" bordered="true"> 
+        <TenthButton
+          @click="() => increment(11)"
+          color="blue"
+          bordered="true"
+        >
           Blueberry (<span>{{ getButtonCounts[11] }}</span>)
         </TenthButton>
       </div>
