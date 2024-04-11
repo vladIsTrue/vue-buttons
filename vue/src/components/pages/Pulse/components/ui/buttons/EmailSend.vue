006d2933ac45bbf086bdb3a2c7366dcdc4a1abc7
vladIsTrue <vladislavurcev@gmail.com>
2024-04-11 21:46:07 +0300

    все еще внизу

--- vue/src/components/pages/Pulse/components/ui/buttons/EmailSend.vue
+++ vue/src/components/pages/Pulse/components/ui/buttons/EmailSend.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="custom-email-input">
     <input type="email" placeholder="Enter your email address here" v-model="email">
-    <button @click="sendEmail" style="background-color: white; border-radius: 50%; padding: 15px 15px; margin : 4px"></button>
+    <button @click="sendEmail"></button>
   </div>
 </template>
 
@@ -21,31 +21,41 @@ export default {
 };
 </script>
 
-<style>
+<style lang="less">
 .custom-email-input {
   position: relative;
   border: 1px solid white;
-  border-radius: 20px;
-  display: flex; /* Добавляем display: flex для центрирования */
-  align-items: center; /* Выравниваем элементы по центру вертикально */
-  justify-content: center; /* Выравниваем элементы по центру горизонтально */
-}
-input[type="email"] {
-  border: none;
-  padding: 10px 20px;
-  width: 400;
-  color: white;
-  background: transparent;
-}
-input[type="email"]::placeholder {
-  color: white;
-}
-input[type="email"]:focus {
-  outline: none;
-}
-button {
-  border: none;
-  color: white;
-  cursor: pointer;
+  border-radius: 50px;
+  display: flex; 
+  align-items: center; 
+  justify-content: space-between;
+  input[type="email"] {
+    border: none;
+    padding: 10px 20px;
+    width: 400;
+    color: white;
+    background: transparent;
+    &::placeholder {
+      color: white;
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+  button {
+    background-color: white;
+    border: none;
+    border-radius: 50%;
+
+    // только найти картинку без фона
+
+    background-image: url('https://w7.pngwing.com/pngs/560/91/png-transparent-arrow-arrowright-next-right-arrows-developer-set-icon.png');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 50%;
+
+    padding: 15px 15px;
+    margin : 4px;
+  }
 }
 </style>
