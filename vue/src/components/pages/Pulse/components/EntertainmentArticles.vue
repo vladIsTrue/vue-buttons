ec3c27e2a5ba6a8e00f948ab1a09ad9ed554dde7
vladIsTrue <vladislavurcev@gmail.com>
2024-05-14 20:43:17 +0300

    лаба 4. fix

--- vue/src/components/pages/Pulse/components/EntertainmentArticles.vue
+++ vue/src/components/pages/Pulse/components/EntertainmentArticles.vue
@@ -68,7 +68,7 @@ export default {
     },
     ...mapActions('pulse', [
       'loadEntertainmentArticlesList'
-    ]),
+    ])
   },
   created () {
     this.loadEntertainmentArticlesList(this.inputData);
